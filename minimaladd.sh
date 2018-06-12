746d2610ebfeaeb12d44d28942d954cd52eed426
Peter Renkema <peter.renkema@home.nl>
2018-06-12 23:35:16 +0200

    minmaladd.sh

--- minimaladd.sh
+++ minimaladd.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
+# get this by installing Git
 # Make sure only root can run our script
 if [ "$(id -u)" != "0" ]; then
    echo "This script must be run as root" 1>&2
    exit 1
 fi
 
+# install minimal extra packages
 yum install -y vim net-tools mlocate wget
 updatedb
+
 
